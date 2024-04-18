c10d84fcc6c069f4e82f0fecbdcc252af97e031b
Lungeanu Marius <34165461+lngmarius@users.noreply.github.com>
2024-04-18 08:39:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 from colorthief import ColorThief
 
-#remind me to never do this again thanks
 
 path =r'D:\python\mar_stricat\poze\dataset\test\R\5.png'
 img = cv.imread(path)
